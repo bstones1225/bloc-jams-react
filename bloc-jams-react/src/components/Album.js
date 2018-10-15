1634cd0b7c428ed1c9c1396717526bc5e7510e7a
Brian Stones <bstones1225@hotmail.com>
2018-10-15 04:07:09 -0500

    Bloc Jams Checkpoint 8 Volume finished

--- bloc-jams-react/src/components/Album.js
+++ bloc-jams-react/src/components/Album.js
@@ -12,28 +12,48 @@ class Album extends Component {
   this.state = {
     album: album,
     currentSong: album.songs[0],
+    currentTime: 0,
+    duration: album.songs[0].duration,
     isPlaying: false,
     isMouseInside:null
   };
-
     this.audioElement = document.createElement('audio');
     this.audioElement.src = album.songs[0].audioSrc;
  }
 
+ componentDidMount() {
+   this.eventListeners = {
+     timeupdate: e => {
+       this.setState({ currentTime: this.audioElement.currentTime });
+     },
+     durationchange: e => {
+       this.setState({ duration: this.audioElement.duration });
+     }
+   };
+   this.audioElement.addEventListener('timeupdate', this.eventListeners.timeupdate);
+   this.audioElement.addEventListener('durationchange', this.eventListeners.durationchange);
+ }
+
+ componentWillUnmount() {
+   this.audioElement.src = null;
+   this.audioElement.removeEventListener('timeupdate', this.eventListeners.timeupdate);
+   this.audioElement.removeEventListener('durationchange', this.eventListeners.durationchange);
+}
+
  play() {
    this.audioElement.play();
    this.setState({ isPlaying: true });
- }
+    }
 
  pause() {
      this.audioElement.pause();
      this.setState({ isPlaying: false });
- }
+    }
 
  setSong(song) {
      this.audioElement.src = song.audioSrc;
      this.setState({ currentSong: song });
-  }
+    }
 
   handleSongClick(song) {
      const isSameSong = this.state.currentSong === song;
@@ -58,6 +78,12 @@ class Album extends Component {
     this.setSong(newSong);
     this.play();
   }
+  handleTimeChange(e) {
+     const newTime = this.audioElement.duration * e.target.value;
+     this.audioElement.currentTime = newTime;
+     this.setState({ currentTime: newTime });
+   }
+
 
 
   mouseEnter = (song) => {
@@ -115,9 +141,12 @@ return (index+1)
           <PlayerBar
             isPlaying={this.state.isPlaying}
             currentSong={this.state.currentSong}
+            currentTime={this.audioElement.currentTime}
+            duration={this.audioElement.duration}
             handleSongClick={() => this.handleSongClick(this.state.currentSong)}
             handlePrevClick={() => this.handlePrevClick()}
             handleNextClick={() => this.handleNextClick()}
+            handleTimeChange={(e) => this.handleTimeChange(e)}
           />
        </section>
      );
