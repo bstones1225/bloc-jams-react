b969258326a01ad3349b7d751f945213c2bca06c
Brian Stones <bstones1225@hotmail.com>
2018-10-15 02:50:19 -0500

    Bloc jams Assignment 8 Player Bar

--- bloc-jams-react/src/components/Album.js
+++ bloc-jams-react/src/components/Album.js
@@ -51,6 +51,14 @@ class Album extends Component {
     this.setSong(newSong);
     this.play();
   }
+  handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max(0, currentIndex + 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+  }
+
 
   mouseEnter = (song) => {
     this.setState({ isMouseInside: song });
@@ -109,6 +117,7 @@ return (index+1)
             currentSong={this.state.currentSong}
             handleSongClick={() => this.handleSongClick(this.state.currentSong)}
             handlePrevClick={() => this.handlePrevClick()}
+            handleNextClick={() => this.handleNextClick()}
           />
        </section>
      );
